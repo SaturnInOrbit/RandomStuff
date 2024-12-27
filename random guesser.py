e7ffeb4ed4ba6a943c3febf25932fef530cd35e5
James <jamesorte089@gmail.com>
2024-12-27 22:57:06 +0800

    okay that's good

--- random guesser.py
+++ random guesser.py
@@ -1,10 +1,30 @@
 import random
+import time
+import os
+game = True
 
-print(random())
-tria = eval(input("Enter amount of triangles: "))
-for x in range(1,8):
-	for y in range(1,tria+1):
-		print(x * "* ",end="\t")
-	print()
+os.system('cls')
+print("Welcome to the guess the number game!")
+print("")
+time.sleep(1)
+print("In this game, you must enter a number between 1-100 and try")
+print("to have your answer be the same as the randomly generated one")
+time.sleep(2)
+print("Go ahead, try it out!")
+print("")
+input("Enter anything to continue...")
+os.system('cls')
 
-print('Okay I think nagana na')
+ran = (random.randint(1,100))
+while game:
+	ans = int(input("Guess the number: "))
+	if ans == ran:
+		print("")
+		print("Well I'll be, you got it!")
+		time.sleep(2)
+		print("That's pretty much it, take care!")
+		break
+	else:
+		print("")
+		print("WRONG, TRY AGAIN!")
+		continue
