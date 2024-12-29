abce30f03f09d87eb05d8db0ddeef820597d8a78
James <jamesorte089@gmail.com>
2024-12-30 00:04:14 +0800

    tests

--- uh.py
+++ uh.py
@@ -1,3 +1,15 @@
 # Placeholder muna kasi gusto lang may maicommit
 # Anyways, ChatGPT bahala sa prompt ko tomorrow
 # So, I've made the guesser. Need nalang tips and tricks
+import random
+import time
+counter = 0
+while True:
+    ran = random.randint(1,1000)
+    print(ran)
+    counter += 1
+    time.sleep(0.001)
+    if counter == 100:
+        break
+    else:
+        continue
