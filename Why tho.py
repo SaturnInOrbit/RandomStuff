d1d72b4e8ce1e875fff68a30ef058891691cab71
James <jamesorte089@gmail.com>
2024-12-31 20:41:25 +0800

    eh

--- Why tho.py
+++ Why tho.py
@@ -5,3 +5,6 @@ print("Hmm...")
 time.sleep(2)
 print("I wonder why 'Hello World' is the de facto start for programming")
 print("I'll have to check that out some time in the future...")
+
+print("12/31/2024")
+print("It's supposed to be a catch all to see if")
