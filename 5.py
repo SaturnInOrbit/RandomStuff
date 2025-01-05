01f795ed58d92d203419b8aa00fe7fa0a2a70a15
James <jamesorte089@gmail.com>
2025-01-05 22:53:37 +0800

    lmao

--- 5.py
+++ 5.py
@@ -1,3 +1,4 @@
 print('I feel alot better now')
 print("I think it really was just the blues")
 print("Ang embarassing pala write ito sa public git")
+print("not like anyone's gonna go through this shi lmao")
